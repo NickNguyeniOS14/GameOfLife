fdb3759ee7e66a0e0d26a0144a9a4e81ea206540
Nick <ptnguyen1901@gmail.com>
2020-07-24 18:27:08 -0400

    set up storyboard

---
+++ GameOfLife/Controllers/AboutViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AboutViewController.swift
+//  GameOfLife
+//
+//  Created by Nick Nguyen on 7/24/20.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
