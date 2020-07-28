6a02e0d01679bcb1dea47ffab04eba92aa665fc0
Nick <ptnguyen1901@gmail.com>
2020-07-28 13:03:04 -0400

    hook up UI

--- GameOfLife/Controllers/GameViewController.swift
+++ GameOfLife/Controllers/GameViewController.swift
@@ -8,10 +8,45 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+  
+  //MARK:- IBOutlets
+  
+  @IBOutlet weak var generationLabel: UILabel!
+  @IBOutlet weak var populationLabel: UILabel!
+  
+  //MARK:- Life Cycle
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-   
+    
   }
+  
+  
+  
+  
+  @IBAction func menuButtonTapped(_ sender: UIBarButtonItem) {
+    print("Menu")
+  }
+  
+  
+  @IBAction func playButtonTapped(_ sender: UIBarButtonItem) {
+    print("Play")
+  }
+  
+  
+  
+  @IBAction func nextButtonTapped(_ sender: UIBarButtonItem) {
+    print("Next")
+  }
+  
+  
+  @IBAction func clearButtonTapped(_ sender: UIBarButtonItem) {
+    print("clear")
+  }
+  
+  
+  
+  
+ 
 }
 
---
+++ GameOfLife/Controllers/SettingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SettingViewController.swift
+//  GameOfLife
+//
+//  Created by Nick Nguyen on 7/28/20.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+  
+  @IBAction func doneTapped(_ sender: UIBarButtonItem) {
+    dismiss(animated: true, completion: nil)
+  }
+  
+  
+  
+  @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+    dismiss(animated: true, completion: nil)
+    
+  }
+  
+  
+  @IBAction func speedValueChanged(_ sender: UISegmentedControl) {
+  }
+  
+  
+  @IBAction func buttonValueChanged(_ sender: UISegmentedControl) {
+  }
+  
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+}
