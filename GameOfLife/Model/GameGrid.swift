51bc77d6ab07153dc2ff537b67c62a6df5dedc12
Nick <ptnguyen1901@gmail.com>
2020-07-28 13:14:46 -0400

    add gamestatsdelegate

---
+++ GameOfLife/Model/GameGrid.swift
@@ -0,0 +1,9 @@
+//
+//  GameGrid.swift
+//  GameOfLife
+//
+//  Created by Nick Nguyen on 7/28/20.
+//
+
+import UIKit
+
---
+++ GameOfLife/Protocol/GameStatsDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  GameStatsDelegate.swift
+//  GameOfLife
+//
+//  Created by Nick Nguyen on 7/28/20.
+//
+
+import Foundation
+
+protocol GameStatsDelegate {
+  func showGeneration()
+  func showPopulation()
+  func gridUpdated()
+}
+
+public enum Patterns {
+  case random
+  
+  // Still Lifes
+  case behive
+  
+  // Oscillators
+  case blinker
+  case toad
+  case beacon
+  case pulsar
+  case pentadecathlon
+  
+  // Spaceships
+  case glider
+}
